3ecd42a55213193436b4a5a076681037fa9ee94e
mrnitesh172-crypto <mrnitesh172@gmail.com>
2025-11-14 13:23:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,419 @@
+# print("Hello")
+
+
+# x="good"
+# def my_function(x):
+#     print(my_list[1:5])
+
+# my_list=["apple","banana","cherry","sjc","herohonda","tata"]
+# my_list[1]="mango"
+# print(my_list)
+
+
+# my_list=["computer","mouse","keyboard","monitor","speaker"]
+# print(my_list[-5:-2])
+# print(my_list[-4:1])
+
+
+# def my_function():
+#     print("python is " + x)
+#     my_function()
+
+
+
+# a="nitesh"
+# b="papa"
+# print(a+b)
+
+
+
+# list
+
+# my_list=["apple","banana"]
+# print(type(my_list))
+# print(my_list)
+
+
+
+# my_list=["apple","banana","cherry",1, 2, "ture"]
+# prin
+
+
+
+
+
+# my_list=["apple","banana", 1,3,5,7]
+
+# my_list[2:5]=["abc"]
+
+# print(my_list)
+
+
+
+
+
+# list1=[1,2,3]
+# list2=[4,5,6]
+# pn=["red"]
+# pn.append("love")
+# print(pn)
+# pn.extend(list2)
+# print(pn)
+
+
+
+#remove
+
+
+
+
+# list1=[1,2,3,"good",0,9]
+
+# list1.remove("good")
+# print(list1)
+# list.pop(list1)
+# print(list1)
+
+#remove
+
+
+
+# list1=[1,2,3,4,5,6]
+
+# list1.insert(2,"apple")
+# print(list1)
+
+# list1.append("banana")
+# print(list1)
+
+# list1.remove("apple")
+# print(list1)
+
+# list1.pop()
+# print(list1)
+
+
+
+
+
+
+
+
+
+# list1=[]
+
+# list1.append
+
+
+
+
+
+
+# loopslists
+
+# list1=[1,2,3,4,4,4,5,6]
+# for x in list1:
+#     print(x)
+
+#sort
+# list=[2,5,1,4,8]
+
+# list.sort()
+
+# print(list)
+# list=[2,5,1,4,8]
+
+# list.sort(reverse=True)
+# print(list)
+
+
+
+# text="hello"
+
+# print(text.upper())
+# print(text.lower())
+# print(text.strip())
+# print(text.replace("hello","world"))
+# print(text.split())
+
+
+
+# x=5
+# y="bdcbk"
+
+#format stating
+
+# Name="bob"
+# age=20
+# print(x+y)
+
+# x="bob"
+# y=20
+# print(f"my name is {x} and my age is {y}")
+
+# X=5
+# X+=3
+
+# print(X)
+
+# x=5
+# y=10
+
+# print(x==y)
+# print(x!=y)
+# print(x>=y)
+# print(x<=y)
+# print(x>y)
+# print(x<y)
+
+
+# x=4
+# y=x
+# z=8
+# # print(y)
+# print(x is y)
+# print(x is not y)
+
+# name={"nitesh","sourav","prashant"}
+# for x in name:
+#     print(x)
+
+
+
+# students={
+#     "name":"chander",
+#     "age":30,
+#     "grades":'A'}
+
+# print(students)
+
+
+
+#if else
+
+# age=20
+ 
+# if age >= 18:
+#     print("you are adult")
+# else:
+#     print("you are not adult")
+
+
+#if alse
+
+# age=16
+
+# if age>18:
+#     print("you are adult")
+# else age==18:
+#     print("you are teenage")
+    
+# else age==16:
+# print("you are 16")
+
+# else:
+# print("you are not adult")
+
+
+# x=15
+# if x>10:
+#     print("x is greater than 10")
+#     if x>20:
+#         print("x is greater than 20")
+#     else:
+#         print("x is not greater than 30")
+
+
+# num = int(input("enter a number"))
+
+# rem = num% 2
+
+# if(rem == 0):
+#     print("even")
+# else:
+#     print("odd")
+
+# age=18
+# if age>=18:
+#     print("it is eligibile for driving")
+# else:1
+#     print("it is not eligible for driving")
+
+# name=["sourav","prashant","kumar"]
+
+
+# word="python"
+# for x in word:
+#     print(x)
+
+
+
+# i = 1
+# while i <= 10:
+#     print(3*i)
+#     i += 1
+
+# for i in range(1,4,):
+#     print(i)
+
+# for j in range(1,3):
+#     print(j)
+#     print(f"i=(i),j=(j)")
+
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     if (i == 3):
+#         i+=1
+#         print("end of loop")
+
+# print even number
+
+
+# i = 1
+# while i <= 10:
+#     if (i % 2 != 0:
+#         i += 1
+#         continue
+#         print
+
+
+# def great():
+
+#     print("Hello,Python!")
+
+
+# great() #calling the function
+
+#function
+
+# def great(name):
+#     print(f"hello ,{name}")
+
+# great("alice")
+# great("bob")
+
+# def add(a,b):
+#     return a + b
+
+# result=add(5,3)
+# print(result) 
+
+# def great(name="student"):
+#     print(f"hello,{name}")
+#     great()
+#     great("alice")
+
+# x=12
+# def my_funs():
+#     y=5
+#     print(x,y)
+
+#     my_funs()
+
+#     print(x)
+#     print(y)
+
+# def greet():
+#     print("Hello!")
+
+# greet()
+
+# def good(sourav):
+#     print(f"livay ,{sourav}")
+
+
+# good("advanture")
+
+# global varibales
+# message = "hello"
+
+# class
+# class car:
+#     def __init__(self,brand,color):
+#        self.brand=brand #Attributs
+#        self.color=color #Attributs
+     
+#     def drive(self):
+#         print(f"{self.color}{self.brand}is driving🚘")
+
+# object
+# car1=car("BMW","Black")
+# car2=car("Tesla","White")
+
+# car1.drive()
+# car2.drive()
+
+
+
+#Array
+
+# import array
+
+# number=array.array[1,2,3,4,5]
+# print(number)
+
+
+# from numpy import random
+
+# x=random.randint(100)
+# print(x)
+
+# x=random.rand()
+# print(x)
+
+# [54]
+# [1,2,3,4,5]
+
+# [{1,2,3},
+#  {4,5,6}]
+# [{7,8,9},
+#  [[10,11,12]]]
+
+# x=random.randint(100,size=(5))
+# print(x)
+
+# x=random.randint(100,size=(3,5))
+# print(x)
+
+# x=random.randint(100,size=(2,3,5))
+# print(x)
+
+# x=random.randint(100,size=(5))
+# print(x)
+
+# x=random.randint(100,size=(2,2,3,5))
+# print(x)
+
+# x=random.choice([4,5,6],size=(5))
+# print(x)
+
+#  pandas
+
+
+# import pandas as pd
+
+# data = [10,20,30,40,50]
+# df = pd.Series(data);
+# print(df)
+
+# data= {
+#     "Name":["John","Alice","Bob"],
+#     "Age":[25,30,22],
+#     "City":["New York","Los Angeles","Chicago"]
+
+# }
+
+# df = pd.DataFrame(data)
+# print(df)
+
+# data={"math":90,"science":85,"english":88}
+# x=pd.Series(data)
+# print(x)
+
+import pandas as pd
+
+x=pd.read_csv("crocodile_dataset.csv")
+print(x.head())
+print(x.tail())
